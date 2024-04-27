ab180a58b39fd9cc7ae39ae4fec6a05d0b6715a8
fei <fei@timesink>
2024-04-27 14:25:05 +0200

    add adw-gtk3

--- arkdep-build.d/timesink-nvidia/extensions/post_install.sh
+++ arkdep-build.d/timesink-nvidia/extensions/post_install.sh
@@ -11,6 +11,12 @@ curl -OL https://github.com/ful1e5/Bibata_Cursor/releases/latest/download/Bibata
 tar -xf Bibata-Modern-Classic.tar.xz --directory /var/tmp/rootfs/usr/share/icons/
 rm -rf Bibata-Modern-Classic.tar.xz
 
+# install adw-gtk3
+mkdir -p /var/tmp/rootfs/usr/share/themes
+curl -OL https://github.com/lassekongo83/adw-gtk3/releases/latest/download/adw-gtk3v5.3.tar.xz
+tar -xf adw-gtk3v5.3.tar.xz --directory /var/tmp/rootfs/usr/share/themes/
+rm -rf adw-gtk3v5.3.tar.xz
+
 # install statically linked nix
 curl -L https://hydra.nixos.org/job/nix/master/buildStatic.x86_64-linux/latest/download-by-type/file/binary-dist > /var/tmp/rootfs/usr/bin/nix
 chmod +x /var/tmp/rootfs/usr/bin/nix
